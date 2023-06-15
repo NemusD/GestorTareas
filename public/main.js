21fa1e9e4c692efd8d8a46d788ebdd2e3714a02d
Ch Droguett <ch.droguett.l@gmail.com>
2023-06-15 02:08:55 -0400

    Se añaden tareas al html

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 //Registro de usuario
-const registerForm = document.querySelector('.register-form');
+/* const registerForm = document.querySelector('.register-form');
 
 registerForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -15,10 +15,10 @@ loginForm.addEventListener('submit', (e) => {
 
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
-});
+}); */
 
 //Creación de tareas
-const createTaskForm = document.querySelector('#form-group');
+const createTaskForm = document.querySelector('#task-form');
 createTaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -27,15 +27,84 @@ createTaskForm.addEventListener('submit', (e) => {
     const status = document.querySelector('#status').value;
     const assignee = document.querySelector('#assignee').value;
 
+    //Crear un objeto
+    const task = {
+        title: title,
+        description: description,
+        status: status,
+        assignee: assignee
+    };
+    //Solicitud POST
+    fetch('/tasks', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    })
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Error al guardar tarea');
+        }
+    })
+    .then(function(data) {
+        const taskCard = createTaskCard(data);
+        const taskList = document.querySelector('#task-list');
+        taskList.appendChild(taskCard);
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
 });
 
 //Obtener todas las tareas
 fetch('tasks')
     .then(response => response.json())
     .then(data => {
-
+        renderTasks(data);
+    })
+    .catch(function(error) {
+        console.log(error);
     });
+    //Renderizar tareas
+    function renderTasks(tasks) {
+        const taskList = document.querySelector('#task-list');
+        taskList.innerHTML = '';
 
+        tasks.forEach(task => {
+            const taskCard = createTaskCard(task);
+            TaskList.appendChild(taskCard);
+        });
+    }
+    //Creación del card de la tarea
+    function createTaskCard(task) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+      
+        const titleElement = document.createElement('h5');
+        titleElement.classList.add('card-title');
+        titleElement.textContent = task.title;
+        card.appendChild(titleElement);
+      
+        const descriptionElement = document.createElement('p');
+        descriptionElement.classList.add('card-description');
+        descriptionElement.textContent = task.description;
+        card.appendChild(descriptionElement);
+      
+        const statusElement = document.createElement('p');
+        statusElement.classList.add('card-status');
+        statusElement.textContent = 'Estado: ' + task.status;
+        card.appendChild(statusElement);
+      
+        const assigneeElement = document.createElement('p');
+        assigneeElement.classList.add('card-responsible');
+        assigneeElement.textContent = 'Responsable: ' + task.assignee;
+        card.appendChild(assigneeElement);
+      
+        return card;
+      }
 //Filtrado de tareas por usuarios
 const userFilter = document.querySelector('#user-filter');
 
--- server.js
+++ server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let tasks = [];
+
 // Configuración del middleware 
 app.use(express.static('public'));
 
-// Ruta
+// Rutas
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.sendFile(__dirname + '/public/index.html');
 });
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+});
+app.get('registro', (req, res) => {
+  res.sendFile(__dirname + '/public/registro.html');
+});
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json('ok');
+})
 
-// Inicia el servidor
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en http://localhost:${port}`);
 });
